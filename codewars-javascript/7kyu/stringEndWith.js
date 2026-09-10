befaf235d9c5739dc4ee0d9ca4a476df7eaf52a5
Phumipat-2005 <215790260+Phumipat-2005@users.noreply.github.com>
2026-09-10 19:28:35 +0700

    feat(7kyu): solve 1 practice problem

---
+++ codewars-javascript/7kyu/stringEndWith.js
@@ -0,0 +1,7 @@
+//Problem: String ends with? (7 kyu)
+//https://www.codewars.com/kata/51f2d1cafc9c0f745c00037d
+
+//--------------Code--------------
+function solution(str, ending){
+  return str.endsWith(ending);
+}
