12e5f6472f63b6e585b9a0129186d659f9b65fb5
Phumipat-2005 <215790260+Phumipat-2005@users.noreply.github.com>
2026-09-19 22:53:41 +0700

    feat(7kyu): solve 1 practice problem

---
+++ codewars-javascript/7kyu/binaryAddition.js
@@ -0,0 +1,7 @@
+//Problem: Binary Addition (7 kyu)
+//https://www.codewars.com/kata/551f37452ff852b7bd000139
+
+//--------------Code--------------
+function addBinary(a, b) {
+  return (a + b).toString(2);
+}
